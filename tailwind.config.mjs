459c960e2550eaa01768b0b85cc160b0dad66341
Herman Ceaser <ceaserbanks09@gmail.com>
2024-01-03 20:49:47 +0300

    added responsive design classes to heropage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: [
+          "Poppins",
+          "Preahvihear",
+          "Raleway",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
       colors: {
         dark: "#11071F",
         light: "#1A0B2E",
+        blue: "#7127BA",
         bunker: {
           100: "#e7eef7",
           200: "#cad9ed",
